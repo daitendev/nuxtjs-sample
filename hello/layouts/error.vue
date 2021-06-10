2dcc29f41b49b228fd585f5aa7b042294b240ca8
daitendev <exiad.iagan@gmail.com>
2021-06-11 00:47:23 +0900

    install google font

---
+++ hello/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 v-if="error.statusCode === 404">404 Error!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["error"],
+};
+</script>
